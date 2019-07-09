06526b463a79a8f3fb1e9ebd12a4e23f58d71bd9
Steve Hu <stevehu@gmail.com>
2019-07-09 13:10:07 -0400

    fixes #60 update Dockerfile and build.sh to image publishing

--- build.sh
+++ build.sh
@@ -42,8 +42,8 @@ cleanup() {
 
 publish() {
     echo "Building Docker image with version $VERSION"
-    docker build -t $IMAGE_NAME:$VERSION -f ./docker/Dockerfile . --no-cache=true
-    docker build -t $IMAGE_NAME:$VERSION-zulu -t $IMAGE_NAME:latest -f ./docker/Dockerfile-Zulu . --no-cache=true
+    docker build -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest -f ./docker/Dockerfile . --no-cache=true
+    docker build -t $IMAGE_NAME:$VERSION-slim -f ./docker/Dockerfile-Slim . --no-cache=true
     echo "Images built with version $VERSION"
     echo "Pushing image to DockerHub"
     docker push $IMAGE_NAME
