1338066ee169ae2d39d0fc2bc8f4629c5c8f68f1
Steve Hu <stevehu@gmail.com>
2022-02-27 19:51:01 -0500

    fixes #141 add a dummy class to force maven release

---
+++ src/main/java/com/networknt/router/Dummy.java
@@ -0,0 +1,16 @@
+package com.networknt.router;
+
+/**
+ * This is a dummy class that to force the fat jar to be released.
+ *
+ * @author Steve Hu
+ */
+public class Dummy {
+    /**
+     * @param name Name
+     */
+    public void sayHello(String name) {
+        ;
+    }
+
+}
