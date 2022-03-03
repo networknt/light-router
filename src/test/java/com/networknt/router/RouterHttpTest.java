e499e10ea1f3036e7031147990a6eb91d40fa29b
Steve Hu <stevehu@gmail.com>
2022-03-03 14:10:19 -0500

    fixes #142 add test case for method rewrite

--- src/test/java/com/networknt/router/RouterHttpTest.java
+++ src/test/java/com/networknt/router/RouterHttpTest.java
@@ -237,6 +237,41 @@ public class RouterHttpTest {
         }
     }
 
+    /**
+     * Calling server1 /v2/address with the DELETE method will rewrite to GET method
+     * @throws Exception
+     */
+    @Test
+    public void testMethodRewrite() throws Exception {
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        try {
+            connection = client.connect(new URI(url), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.create(UndertowOptions.ENABLE_HTTP2, true)).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        try {
+            ClientRequest request = new ClientRequest().setPath("/v2/address").setMethod(Methods.DELETE);
+            request.getRequestHeaders().put(Headers.HOST, "localhost");
+            request.getRequestHeaders().put(HttpStringConstants.SERVICE_ID, "com.networknt.test-1.0.0");
+            connection.sendRequest(request, client.createClientCallback(reference, latch));
+            latch.await();
+        } catch (Exception e) {
+            logger.error("Exception: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        int statusCode = reference.get().getResponseCode();
+        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+        Assertions.assertEquals(200, statusCode);
+        if (statusCode == 200) {
+            Assertions.assertTrue(body.contains("Server"));
+        }
+    }
+
     @Test
     public void testGetServiceIdHeaders() throws Exception {
         final Http2Client client = Http2Client.getInstance();
