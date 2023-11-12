a1ff246f290c59257ba3ce96b962f55abcc7f45b
Steve Hu <stevehu@gmail.com>
2023-11-11 21:14:39 -0500

    fixes #175 refactor based on the ServerConfig change (#176)

--- src/test/java/com/networknt/router/RouterHttpTest.java
+++ src/test/java/com/networknt/router/RouterHttpTest.java
@@ -69,7 +69,7 @@ public class RouterHttpTest {
     static Undertow server2 = null;
     static Undertow server3 = null;
     public static TestServer server = TestServer.getInstance();
-    public static ServerConfig config = (ServerConfig) Config.getInstance().getJsonObjectConfig(CONFIG_NAME, ServerConfig.class);
+    public static ServerConfig config = ServerConfig.getInstance();
     public static Map<String, Object> secret = DecryptUtil.decryptMap(Config.getInstance().getJsonMapConfig(CONFIG_SECRET));
     static SSLContext sslContext = createSSLContext();
 
