526cfa5187229d1a16cbb9d27ae8d19085b389f6
Steve Hu <stevehu@gmail.com>
2024-01-19 14:21:45 -0500

    fixes #180 update test cases based on the HandlerUtils package change (#181)

--- src/test/java/com/networknt/router/middleware/ServiceDictHandlerTest.java
+++ src/test/java/com/networknt/router/middleware/ServiceDictHandlerTest.java
@@ -4,6 +4,7 @@ import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.networknt.handler.HandlerUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
